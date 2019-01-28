b8b3bd668ab8427cad33cd465138a465f23d7bb4
Tomas <tomasiwt@gmail.com>
2019-01-28 22:01:49 +0800

    backup

--- analysis.py
+++ analysis.py
@@ -15,7 +15,7 @@ Firstly, the data in txt file should be input into a database for
 conveniently analyzing. 
 '''
 def to_database(filename):
-    db=create_engine('mysql://root:123456@localhost/cee?charset=utf8',echo=True)
+    db=create_engine('mysql+mysqldb://root:123456@localhost/cee?charset=utf8',echo=True)
     base=declarative_base()
     class university(base):
         __tablename__='ESL of CEE'
@@ -32,6 +32,7 @@ def to_database(filename):
     session=session_class()
     
     with open(filename,'r') as f:
+        n=0
         for i in f.readlines():
             msg=json.loads(i)
             col_data=university( \
@@ -44,13 +45,46 @@ def to_database(filename):
                 average_score=str(msg['average_score']), \
             )
             session.add(col_data)
-            session.commit()
+            n=n+1
+            if n==50:
+                session.commit()
+                n=0
+    session.commit()
+    session.close()
 
 
 
-
+def get_score(colloge_name):
+    db=create_engine('mysql+mysqldb://root:123456@localhost/cee?charset=utf8',echo=True)
+    base=declarative_base()
+    class university(base):
+        __tablename__='ESL of CEE'
+        id=Column(Integer,primary_key=True)
+        colloge=Column(String(64))
+        location=Column(String(64))
+        branch=Column(String(64))
+        batch=Column(String(64))
+        year=Column(String(64))
+        highest_score=Column(String(64))
+        average_score=Column(String(64))
+    Session=sessionmaker(bind=db)
+    session=Session()
+    ret = session.query(university).filter_by(colloge=colloge_name).all()
+    return ret
 
 
 if __name__=='__main__':
     
-    to_database('test1.txt')
+    import matplotlib.pyplot as plt
+    # [to_database('test{}.txt'.format(i)) for i in range(1,5) ]
+    ret=get_score('西南科技大学')
+    
+    plt.figure()
+    x,y=[],[]
+    for i in ret:
+        x.append(float(i.year))
+        y.append(float(i.average_score))
+    plt.scatter(x,y)
+    plt.show()
+
+    
